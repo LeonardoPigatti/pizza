960679a1124a5cdba6db561879c017721dca9e32
Leonardo Pigatti <87331397+LeonardoPigatti@users.noreply.github.com>
2026-03-17 14:04:20 -0300

    Update ModalProduto.jsx

--- src/views/Modal/ModalProduto.jsx
+++ src/views/Modal/ModalProduto.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './Modalpizza.css';
+import './ModalPizza.css';
 
 function formatarPreco(valor) {
   return `R$ ${Number(valor).toFixed(2).replace('.', ',')}`;
